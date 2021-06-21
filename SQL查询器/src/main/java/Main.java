b0501402ac1c0cf7ba14fef930412f731bf9685f
yangjianscut <43467348+yangjianscut@users.noreply.github.com>
2021-06-21 23:45:51 +0800

    3th assignment

--- SQL查询器/src/main/java/Main.java
+++ SQL查询器/src/main/java/Main.java
@@ -2,6 +2,6 @@ public class Main {
     public static void main(String[] args) {
         //创建一个窗口，创建一个窗口
           new Connector();
-       
+
     }
 }
---
+++ 数据在线计算/src/main/scala/BuyRecord.scala
@@ -0,0 +1,11 @@
+import scala.beans.BeanProperty
+
+case class BuyRecord(@BeanProperty var buy_time:String,
+                     @BeanProperty var buy_address:String,
+                     @BeanProperty var origin:String,
+                     @BeanProperty var destination:String,
+                     @BeanProperty var username:String){
+  def this() = {
+    this(null, null, null, null,null)
+  }
+}
